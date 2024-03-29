a059358508e4e24d5b77124965fcca51bfe3e7e3
tichdog <ty2005r75@mail.ru>
2024-03-29 13:22:13 +0300

    add test system line

--- TheNervousSystem/Assets/TEST/LinePointBtn.cs
+++ TheNervousSystem/Assets/TEST/LinePointBtn.cs
@@ -1,28 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class LinePointBtn : MonoBehaviour
 {
-    public LineRenderer line;
+    public int count = 0;
+    public float startWidth = 1.0f;
+    public float endWidth = 1.0f;
+    public List<int> point;
 
+    private LineRenderer line;
+    private bool flag = true;
     public void Start()
     {
-        //line = GetComponent<LineRenderer>();
-        /*line.startWidth = 1.2f;
-        line.endWidth = 1.2f;*/
+        line = GetComponent<LineRenderer>();
+        line.startWidth = startWidth;
+        line.endWidth = endWidth;
         line.positionCount = 0; // Обнуляем точки
     }
 
     public void _click(GameObject obj)
     {
-        // achivOneStep.transform.Find("on").gameObject.SetActive(true);
-        Vector2 temp;
-        temp.x = obj.transform.position.x;
-        temp.y = obj.transform.position.y;
-        Debug.Log(temp.x + " vector " + temp.y);
-        line.positionCount++;
-        Debug.Log(line.positionCount + " positionCNT");
-        line.SetPosition(line.positionCount - 1, temp);
+        if(point.IndexOf(int.Parse(obj.name)) == -1)
+        {
+            Vector3 temp;
+            // Button position
+            temp.x = obj.transform.position.x;
+            temp.y = obj.transform.position.y;
+            temp.z = 1;
+
+            // add number button to list
+            point.Add(int.Parse(obj.name));
+
+            line.positionCount++;
+            line.SetPosition(line.positionCount - 1, temp);
+        
+            Debug.Log(line.positionCount + " positionCNT");
+            Debug.Log(temp.x + " vector " + temp.y);
+        }
+        else
+        {
+            Debug.Log("Element copy");
+        }
+
+        if(point.Count == count)
+        {
+            for(int i = 0;  i < point.Count - 1; i++)
+            {
+                if (point[i] > point[i + 1])
+                {
+                    flag = false;
+                    _resetPoint();
+                    break;
+                }
+            }
+
+            if (flag)
+            {
+                Debug.Log("Уровень пройден");
+            }
+            else
+            {
+                Debug.Log("ТЫ еблан");
+            }
+        }
+    }
+
+    public void _resetPoint()
+    {
+        point.Clear();
+        line.positionCount = 0;
     }
 }
