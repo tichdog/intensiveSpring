849786c200fbb1198f6499e6147153baeb5f3274
tichdog <ty2005r75@mail.ru>
2024-03-30 22:26:16 +0300

    add

--- TheNervousSystem/Assets/C#/Game/LoadDifficulty.cs
+++ TheNervousSystem/Assets/C#/Game/LoadDifficulty.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class LoadDifficulty : MonoBehaviour
 {
     public GameObject helpBtn;
-    
+    public GameObject Timer;
     public void Start()
     {
         if(PlayerPrefs.GetInt("diff") == 0)
         {
             helpBtn.SetActive(true);
+            Timer.SetActive(false);
         }
         else
         {
+            Timer.SetActive(true);
             helpBtn.SetActive(false);
         }
     }
--- TheNervousSystem/Assets/TEST/LinePointBtn.cs
+++ TheNervousSystem/Assets/TEST/LinePointBtn.cs
@@ -5,6 +5,7 @@ using UnityEngine.Experimental.GlobalIllumination;
 
 public class LinePointBtn : MonoBehaviour
 {
+    public string nameCreature = "";
     public int count = 0;
     public float startWidth = 1.0f;
     public float endWidth = 1.0f;
@@ -12,8 +13,42 @@ public class LinePointBtn : MonoBehaviour
 
     private LineRenderer line;
     private bool flag = true;
+
+    private int id = 0;
     public void Start()
     {
+        switch (nameCreature)
+        {
+            case "worm":
+            {
+                id = 1;
+                break;
+            }
+
+            case "grasshopper":
+                {
+                    id = 2;
+                    break;
+                }
+
+            case "frog":
+                {
+                    id = 3;
+                    break;
+                }
+
+            case "hydra":
+                {
+                    id = 0;
+                    break;
+                }
+
+            case "human":
+                {
+                    id = 4;
+                    break;
+                }
+        }
         line = GetComponent<LineRenderer>();
         line.startWidth = startWidth;
         line.endWidth = endWidth;
@@ -46,24 +81,37 @@ public class LinePointBtn : MonoBehaviour
 
         if(point.Count == count)
         {
+            check();
+        }
+    }
+
+    private void check()
+    {
+        if(id == 1 || id == 2)
+        {
             for(int i = 0;  i < point.Count - 1; i++)
             {
+                Debug.Log(point[i] + " i ");
                 if (point[i] > point[i + 1])
                 {
                     flag = false;
                     _resetPoint();
                     break;
                 }
+                else
+                {
+                    flag = true;
+                }
             }
-
-            if (flag)
-            {
-                Debug.Log("Уровень пройден");
-            }
-            else
-            {
-                Debug.Log("ТЫ еблан");
-            }
+        }
+        
+        if (flag)
+        {
+            Debug.Log("Уровень пройден");
+        }
+        else
+        {
+            Debug.Log("fail");
         }
     }
 
