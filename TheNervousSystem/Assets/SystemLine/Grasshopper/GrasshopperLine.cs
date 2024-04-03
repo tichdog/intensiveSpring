2037f1ae766618b4e17910b238bd968803791333
tichdog <ty2005r75@mail.ru>
2024-04-03 04:31:57 +0300

    add lvlTwo

---
+++ TheNervousSystem/Assets/SystemLine/Grasshopper/GrasshopperLine.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class GrasshopperLine : MonoBehaviour
+{
+    // Счетчик для линий
+    public int count = 0;
+
+    public List<int> point;
+
+    public GameObject sys;
+    public GameObject elem;
+
+    public static Vector3 one;
+    public static Vector3 two;
+
+    public bool lvl = false;
+
+    public Animator animator;
+    public GameObject timePanel;
+    public GameObject btn;
+
+    private string key_1 = "1234";
+
+    private bool flag = false;
+
+    public void Start()
+    {
+        btn.SetActive(false);
+    }
+
+    public void _click(GameObject obj)
+    {
+        string name = obj.name;
+        int temp = int.Parse(name);
+        point.Add(temp);
+
+        if (!flag)
+        {
+            one = GetPos(obj);
+            flag = true;
+        }
+        else
+        {
+            two = GetPos(obj);
+
+            if(one.x == two.x || one.y == two.y)
+            {
+                flag = false;
+                try
+                {
+                    point.RemoveAt(point.Count - 1);
+                    point.RemoveAt(point.Count - 1);
+                }
+                catch
+                {
+                    Debug.Log("Fatal ERROR!");
+                }
+                return;
+            }
+
+            sys = Instantiate(elem);
+            flag = false;
+        }
+
+        if(point.Count == count)
+        {
+            check();
+        }   
+    }
+
+    public void check()
+    {
+        string s = "";
+        int temp = 0;
+
+        foreach(int i in point)
+        {
+            if(i != temp)
+            {
+                s += i;
+                temp = i;
+            }
+        }
+
+        Debug.Log(s);
+
+        if(s.IndexOf(key_1) != -1)
+        {
+            GameObject[] obj;
+            obj = GameObject.FindGameObjectsWithTag("point");
+            foreach (GameObject i in obj)
+            {
+                Destroy(i);
+            }
+
+            lvl = true;
+            timePanel.SetActive(false);   
+            
+            // Чек ачивки
+            if(PlayerPrefs.GetInt("diff") == 0)
+            {
+                PlayerPrefs.SetInt("lvl2", 1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt("lvl2", 2);
+            }
+
+            animator.Play("compl");
+            Invoke("_btn",0.8f);
+            Debug.Log("LVL COMPLETE");
+        }
+        else
+        {
+            lvl = false;
+            _reset();
+            Debug.Log("FAIL");
+        }
+
+    }
+
+    public void _btn()
+    {
+        btn.SetActive(true);
+    }
+
+    public static Vector3 coordinateOne()
+    {
+        return one;
+    }
+
+    public static Vector3 coordinateTwo()
+    {
+        return two;
+    }
+
+    public Vector3 GetPos(GameObject obj)
+    {
+        Vector3 temp;
+        // Button position
+        temp.x = obj.transform.position.x;
+        temp.y = obj.transform.position.y;
+        temp.z = 1;
+
+        return temp;
+    }
+    
+    public void _reset()
+    {
+        point.Clear();
+        GameObject[] obj;
+        obj = GameObject.FindGameObjectsWithTag("point");
+        foreach(GameObject i in obj)
+        {
+            Destroy(i);
+        }
+    }
+
+    public void _continue()
+    {
+        animator.Play("finich");
+        btn.SetActive(false);
+    }
+}
---
+++ TheNervousSystem/Assets/SystemLine/Grasshopper/grasshopper.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.TerrainTools;
+
+public class grasshopper : MonoBehaviour
+{
+    private LineRenderer line;
+    static Vector3 one = Vector3.zero;
+    static Vector3 two = Vector3.zero;
+    public void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.startWidth = 0.2f;
+        line.endWidth = 0.2f;
+        line.positionCount = 0; // Обнуляем точки
+        // !!!
+        one = GrasshopperLine.coordinateOne();
+        two = GrasshopperLine.coordinateTwo();
+
+        paint();
+
+    }
+
+    public void paint()
+    {
+        line.positionCount++;
+        line.SetPosition(line.positionCount - 1, one);
+        line.positionCount++;
+        line.SetPosition(line.positionCount - 1, two);
+    }
+}
