3cb88ca6b6dfe8fa7fdb505933bf68d9f66036f8
tichdog <ty2005r75@mail.ru>
2024-04-16 17:56:17 +0300

    v1.0

--- TheNervousSystem/Assets/C#/Comm&cheat/Comics.cs
+++ TheNervousSystem/Assets/C#/Comm&cheat/Comics.cs
@@ -4,63 +4,44 @@ using UnityEngine;
 
 public class Comics : MonoBehaviour
 {
-    public GameObject comics;
-
-    public GameObject l1;
-    public GameObject l2;
-    public GameObject l3;
+    public Animator animator;
+    public GameObject[] lists;
 
     private int t;
     public void Start()
     {
-        t = 1;
-        comics.SetActive(false);
-
-        l1.SetActive(false);
-        l2.SetActive(false);
-        l3.SetActive(false);
+        t = 0;
+        foreach (GameObject i in lists)
+        {
+            i.SetActive(false);
+        }
     }
 
     public void _right()
     {
-        if(t == 1)
+        if(t < lists.Length - 1)
         {
-            t = 2;
-            l1.SetActive(false);
-            l2.SetActive(true);
-        }
-        else if(t == 2)
-        {
-            t = 3;
-            l2.SetActive(false);
-            l3.SetActive(true);
+            t++;
+            lists[t].SetActive(true);         
         }
     }
 
     public void _left()
     {
-        if(t == 3)
-        {
-            t = 2;
-            l3.SetActive(false);
-            l2.SetActive(true);
-        }
-        else if( t == 2)
-        {
-            t = 1;
-            l2.SetActive(false);
-            l1.SetActive(true);
-        }
+       if(t != 0)
+       {
+            lists[t].SetActive(false);
+            t--;    
+       }
     }
     public void _open()
     {
-        comics.SetActive (true);
-        l1.SetActive(true);
-        
+        animator.Play("open");
+        lists[t].SetActive(true);
     }
 
     public void _close()
     {
-        comics.SetActive (false);
+        animator.Play("close");
     }
 }
